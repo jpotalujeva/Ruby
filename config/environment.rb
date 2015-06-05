b0426040fafa75a87ba8874cc8e37eb897ee21ef
jpotalujeva <jpotalujeva@gmail.com>
2015-06-05 13:59:45 +0300

    changes in mailer

---
+++ config/environment.rb
@@ -0,0 +1,17 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+# ActionMailer::Base.delivery_method = :smtp
+
+# ActionMailer::Base.server_settings = {
+#    :address => "jpotalujeva@gmail.com",
+#    :domain => "gmail.com",
+#    :authentication => :login,
+#    :user_name => "jpotalujeva",
+#    :password => "juze1616",
+# }
+
+# ActionMailer::Base.default_content_type = "text/html"
